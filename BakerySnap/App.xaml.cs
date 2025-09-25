f590cf05006942ef7758b22a3bec603d2dbe71b7
Manou Dierckx <r0231646@student.thomasmore.be>
2025-09-25 11:22:08 +0200

    Add project files.

---
+++ BakerySnap/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace BakerySnap
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ BakerySnap/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace BakerySnap
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
